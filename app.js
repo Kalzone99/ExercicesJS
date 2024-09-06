8253546b3c309563c53345697c4cca82fccb5927
Kevin Selvais <kalzone99@gmail.com>
2024-09-06 11:05:50 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,113 @@
+//alert('Hello There');
+//console.log("I am in the console");
+
+//let myName = 'Kevin';
+//console.log(myName);
+//alert(myName);
+
+//let exancheRate = 0.93;
+//console.log(50 * exancheRate);
+//console.log(132 * exancheRate);
+//exancheRate = 0.91;
+//console.log(50 * exancheRate);
+//console.log(132 * exancheRate);
+
+//04B.practice
+//var a = "3";
+//var b = "8";
+/***********Do not change the code above 👆*******/
+//Write your code here :
+//var c = a;
+//a = b;
+//b = c;
+/***********Do not change the code below 👇*******/
+//console.log("a is " + a);
+//console.log("b is " + b);
+
+//06B.practice
+// DON'T TOUCH THIS LINE!
+//const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+
+// YOUR CODE BELOW THIS LINE:
+//console.log(message.toLowerCase().trim());
+/*
+let word = "Dumbledore";
+let lastIndex = word.length -1
+console.log(word[lastIndex]);
+word = "supercalifragilisispektiquelquechoz";
+lastIndex = word.length -1;
+console.log(word[lastIndex]);
+
+Not very happy with this one tho 
+*/
+/*
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+let facialHair = word.slice(5,10).replace("o", "e");
+
+console.log(facialHair);
+*/
+
+//07Numbers oh my float
+
+/*let x = Math.floor(Math.random() * 11);
+console.log(x);
+let y = Math.random() * 4 + 1;
+console.log(y)
+*/
+
+//08.Practice
+/*const nomination = prompt("What is your name?");
+const age = prompt("How old are you?");
+const home = prompt("Where do you live?");
+
+alert(`This here is ${nomination}, she/he is ${age} years old, and is coming from ${home}`);
+*/
+/*const ask = prompt("Enter the price without VAT, please: ");
+let vat = parseFloat(ask)+ parseFloat(ask) * 21 / 100;
+vat = vat.toFixed(2);
+alert(`The price including VAT is ${vat}`);
+*/
+/*const radius = prompt("Enter the radius of the circle: ");
+const pi = 3.14
+let surface = radius ** 2 * pi
+alert(`The surface of the circle is ${surface}`);
+*/
+//Addition
+/*const firstNumber = prompt("Enter the first number: ");
+const secondNumber = prompt("Enter second number: ");
+let addition = parseFloat(firstNumber) + parseFloat(secondNumber);
+alert(`The result is:  ${addition}` );*/
+//Substraction
+/*const firstNumber = prompt("Enter the first number: ");
+const secondNumber = prompt("Enter second number: ");
+let substraction = parseFloat(firstNumber) - parseFloat(secondNumber);
+alert(`The result is:  ${substraction}` );*/
+//Multiplication
+/*const firstNumber = prompt("Enter the first number: ");
+const secondNumber = prompt("Enter second number: ");
+let multiplication = parseFloat(firstNumber) * parseFloat(secondNumber);
+alert(`The result is:  ${multiplication}` );*/
+//Division
+/*const firstNumber = prompt("Enter the first number: ");
+const secondNumber = prompt("Enter second number: ");
+let division = parseFloat(firstNumber) / parseFloat(secondNumber);
+alert(`The result is:  ${division}` );*/
+//all of the above
+/*const firstNumber = prompt("Enter the first number: ");
+const secondNumber = prompt("Enter second number: ");
+let addition = parseFloat(firstNumber) + parseFloat(secondNumber);
+let substraction = parseFloat(firstNumber) - parseFloat(secondNumber);
+let multiplication = parseFloat(firstNumber) * parseFloat(secondNumber);
+let division = parseFloat(firstNumber) / parseFloat(secondNumber);
+alert(`${firstNumber} + ${secondNumber} = ${addition}\n${firstNumber} - ${secondNumber} = ${substraction}\n${firstNumber} * ${secondNumber} = ${multiplication}\n${firstNumber} / ${secondNumber} = ${division}`);
+*/
+
+
+
+
+
+
+
